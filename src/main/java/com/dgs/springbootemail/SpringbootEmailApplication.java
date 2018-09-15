0f169657c8e6100563523eb751894cdbd860f0b9
Dragos Sarpe <contact@usdgadget.com>
2018-09-15 18:34:03 +0300

    Initial commit

---
+++ src/main/java/com/dgs/springbootemail/SpringbootEmailApplication.java
@@ -0,0 +1,12 @@
+package com.dgs.springbootemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEmailApplication.class, args);
+    }
+}
